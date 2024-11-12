7d90033dc315f5bd603012bde98ff5693b6a44d1
naimurnemu <naimurrahaman82@gmail.com>
2024-11-12 19:06:05 +0600

    add structure that how to call component

--- src/pages/Public/LandingPage/LandingPage.jsx
+++ src/pages/Public/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { ExampleComp } from '../../../components';
 
 const LandingPage = () => {
   return (
     <div>
       <h2>This is the main landing page</h2>
+      <ExampleComp />
     </div>
   );
 };
