52f211ca411d44462fb43b0a965020968d5bd4d5
rashed <mdrashedbiswas83@gmail.com>
2024-11-22 23:47:03 +0600

    Routing the join now button for register page and updated some style

--- src/components/RunningClub/RunningClub.jsx
+++ src/components/RunningClub/RunningClub.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import blogImg from "../../assets/images/blogImg.jpeg";
+import { Link } from "react-router-dom";
 
 const JoinClub = () => {
   return (
@@ -7,7 +8,7 @@ const JoinClub = () => {
       className="lg:h-screen bg-fixed bg-center bg-cover flex items-center pb-8"
       style={{ backgroundImage: `url(${blogImg})` }}
     >
-      <div className=" text-white space-y-6 pl-10 lg:pl-20">
+      <div className=" text-white lg:space-y-10 space-y-5 pl-10 lg:pl-20">
         <h1 className="text-2xl lg:text-4xl font-bold text-white">
           Join Our Running Club Now <span className="bg-[#C3E92D] rounded-full px-2 text-black">-30%</span>
         </h1>
@@ -15,9 +16,11 @@ const JoinClub = () => {
             <h1 className="lg:text-7xl text-3xl font-semibold">RUNNING CLUB</h1>
         </div>
         <div>
-          <button className="bg-[#C3E92D] hover:bg-black hover:text-[#C3E92D] text-black font-medium lg:py-3 py-2 px-6  rounded-md">
+         <Link to="/register">
+         <button className="bg-[#C3E92D] hover:bg-black hover:text-[#C3E92D] text-black font-medium lg:py-3 py-2 px-6  rounded-md">
             JOIN NOW
           </button>
+         </Link>
         </div>
       </div>
     </div>
