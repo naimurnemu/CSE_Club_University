28bea7c6c3577d43fb19114b37efeed9ebd879f6
Yeasaleh <yeasalehofficial2002@gmail.com>
2024-11-27 19:29:23 +0600

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import { Footer, Header } from "./components";
 
 function App() {
+  let pathname = window.location.pathname;
+
   return (
     <div className="mx-auto">
       <Header />
       <div className="min-h-[calc(100vh-100px)]">
         <Outlet />
       </div>
-      <Footer />
+      {pathname == "/chat" ? "" : <Footer />}
     </div>
   );
 }
